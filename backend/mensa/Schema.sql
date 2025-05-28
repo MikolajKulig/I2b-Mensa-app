f4eb28d88ad54c41259f37b64870bc217fa416a0
MikolajKulig <mikolaj.kulig08@gmail.com>
2025-05-28 15:38:54 +0200

    created Maven Project, added table schema.sql

--- backend/mensa/Schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE products (
-    productID SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    price DECIMAL(10, 2) NOT NULL,
-    available BOOLEAN NOT NULL DEFAULT TRUE,
-);
